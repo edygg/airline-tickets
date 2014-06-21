29fac2981f1fa0cf49958ec309cecc47e24f3446
Henry23 <gabriel_23hgap@unitec.edu>
2014-06-21 15:16:49 -0600

    saving tickets

--- db/schema.rb
+++ db/schema.rb
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20140614203940) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
+  
   create_table "types", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
