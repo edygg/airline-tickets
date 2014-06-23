e2f80288b6f6cb53e4c7e7f9e58e8929a335cdf1
Josue Rojas <josue.rojas@unitec.edu>
2014-06-23 04:10:46 +0000

    Exportando BD Despues del Cagadal

--- SQLs/airlines_development.sql
+++ SQLs/airlines_development.sql
@@ -224,29 +224,6 @@ LOCK TABLES `schedules_tickets` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `schema_migrations`
---
-
-DROP TABLE IF EXISTS `schema_migrations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `schema_migrations` (
-  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  UNIQUE KEY `unique_schema_migrations` (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `schema_migrations`
---
-
-LOCK TABLES `schema_migrations` WRITE;
-/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
-INSERT INTO `schema_migrations` VALUES ('20140613024553'),('20140613024614'),('20140613024808'),('20140613044343'),('20140613044514'),('20140613052753'),('20140613055622'),('20140614202035'),('20140614203940');
-/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tickets`
 --
 
@@ -283,4 +260,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-23  3:31:56
+-- Dump completed on 2014-06-23  4:10:05
