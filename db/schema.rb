9ebbd2492e95b9ac0560eafc75a2c495e40e4044
Josue Rojas <josue.rojas@unitec.edu>
2014-06-23 08:09:14 +0000

    Agregando mas campos al formulario de busqueda

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614203940) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "airplanes", force: true do |t|
     t.string   "airplane_model"
