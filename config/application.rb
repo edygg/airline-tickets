a7d1748dc4db7550a3d52abed850908b1786e9b4
Henry23 <gabriel_23hgap@unitec.edu>
2014-06-23 13:03:19 -0600

    updates

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Airlines
   class Application < Rails::Application
-    config.force_ssl = false
+    #config.force_ssl = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
