f2bd656cf23af2122121f4848a49e0e7d1690c3f
Henry23 <gabriel_23hgap@unitec.edu>
2014-06-23 10:57:05 -0600

    update

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,20 @@
 class FlightsController < ApplicationController
 	def show
-		@departure = City.find_by_name(params[:departure_city_id]).id 
-		@arrival =  City.find_by_name(params[:arrival_city_id]).id
-		@Schedules = Schedule.find_all_by_departure_city_id_and_arrival_city_id(@departure, @arrival)
+    if City.find_by_name(params[:departure_city_id]).try(:id) and City.find_by_name(params[:arrival_city_id]).try(:id)
+		  @departure = City.find_by_name(params[:departure_city_id]).id 
+		  @arrival =  City.find_by_name(params[:arrival_city_id]).id
+
+      if @departure == @arrival
+        redirect_to flights_path, notice: 'Enter different cities'
+      end
+    else 
+        redirect_to flights_path, notice: 'Path not found' 
+    end
+
+
+
+    @Schedules = Schedule.find(:all, :conditions => ["departure_city_id = ? and arrival_city_id in (?)", @departure, @arrival], :order => 'price ASC')
+
     
     @classTypeId = params[:class_type]
     #@classType = ClassType.find(classTypeId.to_s).name
